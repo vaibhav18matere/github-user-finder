eaae4ec78106f19cc01d62b10725583bb503aabf
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2021-09-29 02:23:28 +0530

    destructuring of navbar

--- todolists/src/Components/Layout/Navbar.js
+++ todolists/src/Components/Layout/Navbar.js
@@ -1,27 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types';
 
-export class Navbar extends Component {
-
-    static defaultProps = {
-        title: "github finder",
-        icon: "fab fa-github"
-    }
-
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired
-    };
-
-    render() {
-        return (
-            <nav className="navbar bg-primary">
-                <h1>
-                    {this.props.title}
-                </h1>
-            </nav>
-        )
-    }
+const Navbar = (props) => {
+    return (
+        <nav className="navbar bg-primary">
+            <h1>
+                {props.title}
+            </h1>
+        </nav>
+    )
 }
+
+Navbar.defaultProps = {
+    title: "github finder"
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired
+};
+
 
 export default Navbar
--- todolists/src/Components/User/UserItems.js
+++ todolists/src/Components/User/UserItems.js
@@ -1,10 +1,7 @@
 import React from 'react'
 
 const UserItems = (props) => {
-
-
     const { login, avatar_url, html_url } = props.user;
-
     return (
         <div className="card text-center">
             <img src={avatar_url} alt="" className="round-img" style={{ width: "60px" }} />
@@ -13,7 +10,6 @@ const UserItems = (props) => {
             <div>
                 <a href={html_url} className="btn btn-dark my-1">Click</a>
             </div>
-
         </div>
 
     );
