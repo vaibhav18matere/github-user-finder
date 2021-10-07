6d76fc47f05617c0c649ee9d8021ca667e02a992
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2021-10-08 02:35:23 +0530

    feat : about section added (React Router)

--- todolists/src/Components/Layout/Navbar.js
+++ todolists/src/Components/Layout/Navbar.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ title }) => {
     return (
         <nav className="navbar bg-primary">
-            <h1>
-                {title}
-            </h1>
+            <h1>{title}</h1>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+            </ul>
         </nav>
-    )
-}
+    );
+};
 
 Navbar.defaultProps = {
     title: "github finder"
@@ -18,6 +25,5 @@ Navbar.defaultProps = {
 Navbar.propTypes = {
     title: PropTypes.string.isRequired
 };
-
 
 export default Navbar
